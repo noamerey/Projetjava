299d8bf3390467c423f0b0df4fb092e72d8cd543
RainderAx <114318268+RainderAx@users.noreply.github.com>
2023-01-10 16:11:48 +0100

    Update apiRouter.js

--- apiRouter.js
+++ apiRouter.js
@@ -19,6 +19,9 @@ exports.router = (function() {
   apiRouter.route('/messages/new/').post(messagesCtrl.createMessage);
   apiRouter.route('/messages/').get(messagesCtrl.listMessages);
 
+  // Moteur de recherche
+  apiRouteur.route('/recherche/)
+                   
   // Likes
  
   apiRouter.route('/messages/:messageId/vote/like').post(likesCtrl.likePost);
