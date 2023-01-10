26daa29cc9edf9d933c281fce631da1ce71709cb
RainderAx <114318268+RainderAx@users.noreply.github.com>
2023-01-10 15:48:57 +0100

    Add files via upload

--- routes/motor.js
+++ routes/motor.js
@@ -1,26 +1,29 @@
 // Imports
 var models   = require('../models');
-var jwtUtils = require('../utils/jwt.utils');
 var asyncLib = require('async');
+var jwtUtils = require('../utils/jwt.utils');
 
-// Envoi de la requête de recherche à l'API
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://localhost:6677/search?q=keyword&filter=value');
-xhr.setRequestHeader('Authorization', 'Bearer YOUR_ACCESS_TOKEN');
-xhr.onload = function() {
-  if (xhr.status === 200) {
-    const results = processResults(xhr.responseText);
-  } else {
-    console.error('Error searching:', xhr.status);
-  }
-};
-xhr.send();
+// Routes
+module.exports = {
+  motor: function(req, res) {
+    
+    var headerAuth  = req.headers['authorization'];
+    var userId      = jwtUtils.getUserId(headerAuth);
 
-// Fonction de traitement des résultats de la recherche
-function processResults(response) {
- 
-  return results;
+    // Params
+       asyncLib.waterfall([
+      function(done) {
+        models.User.findOne({
+          where: { id: userId }
+        })
+        .then(function(userFound) {
+          done(null, userFound);
+        })
+        .catch(function(err) {
+          return res.status(500).json({ 'error': 'unable to verify user' });
+        });
+      },
+    ], );
+  },
 }
-
-
 
