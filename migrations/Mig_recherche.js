ad42f717aab8293095f8b59f3cb41dc7afb1e374
RainderAx <114318268+RainderAx@users.noreply.github.com>
2023-01-10 15:37:40 +0100

    Update Mig_recherche.js

--- migrations/Mig_recherche.js
+++ migrations/Mig_recherche.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('recherche', {
-      id: {
+      RsId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
