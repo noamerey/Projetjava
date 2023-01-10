2581cf07d52fe6fdce486f33d96210c9d64eef18
RainderAx <114318268+RainderAx@users.noreply.github.com>
2023-01-10 15:23:26 +0100

    Add files via upload

---
+++ migrations/Mig_recherche.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recherche', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+     
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('recherche');
+  }
+};
